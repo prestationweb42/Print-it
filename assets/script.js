00c1fda8cf18f6e8cdc580a0d50e6733a72dc3c2
prestationweb42 <dani.vent@orange.fr>
2023-05-24 22:29:14 +0200

    ajout fonction carrousel

--- assets/script.js
+++ assets/script.js
@@ -19,16 +19,46 @@ const slides = [
     },
 ];
 //
+const banner_img = document.querySelector(".banner-img");
 const arrow_left = document.querySelector(".arrow_left");
 const arrow_right = document.querySelector(".arrow_right");
 const div_dots = document.querySelector(".dots");
+let count = 0;
+const nb_slides = slides.length;
+console.log(nb_slides);
+// previous
+function previous() {
+    if (count > 0) {
+        banner_img.innerHTML =
+            '<img src="./assets/images/slideshow/' +
+            slides[count].image +
+            '" />' +
+            "<p>" +
+            slides[count].tagLine +
+            "</p>";
+        count--;
+    } else {
+        count = nb_slides - 1;
+    }
+}
+arrow_left.addEventListener("click", previous);
+// next
+function next() {
+    if (count < nb_slides) {
+        banner_img.innerHTML =
+            '<img src="./assets/images/slideshow/' +
+            slides[count].image +
+            '" />' +
+            "<p>" +
+            slides[count].tagLine +
+            "</p>";
+        count++;
+    } else {
+        count = 0;
+    }
+}
+arrow_right.addEventListener("click", next);
 //
-arrow_left.addEventListener("click", () => {
-    alert("Fleche gauche");
-});
-arrow_right.addEventListener("click", () => {
-    alert("Fleche droite");
-});
 for (let i = 0; i < slides.length; i++) {
     const dot = document.createElement("div");
     dot.classList.add("dot");
