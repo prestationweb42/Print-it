133d974d72dc82ddb04ec21b28bc2c6be02cd98a
prestationweb42 <dani.vent@orange.fr>
2023-06-02 11:09:19 +0200

    resize img webp

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,20 @@
 const slides = [
     {
-        image: "slide1.jpg",
+        image: "slide1.webp",
         tagLine:
             "Impressions tous formats <span>en boutique et en ligne</span>",
     },
     {
-        image: "slide2.jpg",
+        image: "slide2.webp",
         tagLine:
             "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
     },
     {
-        image: "slide3.jpg",
+        image: "slide3.webp",
         tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
     },
     {
-        image: "slide4.png",
+        image: "slide4.webp",
         tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
     },
 ];
